31f8a397541e597593ada37b07b663ecf73ba48e
apktool <apktool@gmail.com>
2017-11-08 10:41:34 +0800

    Change detail templates

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 #from django.shortcuts import render
-from django.http import Http404
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import Question
 
@@ -18,7 +18,7 @@ def index(request):
 
 def detail(request, question_id):
     try:
-        question = Question.objects.get(pk=question_id)
+        question = get_object_or_404(Question, pk=question_id)
     except Question.DoesNotExist:
         raise Http404('Question does not exist')
     return render(request, 'polls/detail.html', {'question': question})
