361fef522a423872898233d2a205dbb5d4e02463
apktool <apktool@gmail.com>
2017-11-11 09:46:03 +0800

    Fix some bug

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 def index(request):
@@ -25,9 +25,22 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    print(request.POST)
+    print('---------------')
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST.get('choice', '1'))
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You didn\'t select a choice.',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
